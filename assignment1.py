03e8c3a2ce0367f6d3d2a1a8707d380ae24bf854
saicg0904-cloud <saicg0904@gmail.com>
2026-03-08 21:04:41 +1100

    add add_place with input validation

--- assignment1.py
+++ assignment1.py
@@ -86,6 +86,36 @@ def display_places(places):
     )
 
 
+def get_valid_input(prompt, is_blank_allowed=False):
+    """Generic input validation for non-numeric inputs (name/country)."""
+    while True:
+        user_input = input(prompt).strip()
+        if user_input or is_blank_allowed:
+            return user_input
+        print("Input can not be blank")
+
+
+def get_valid_number(prompt, min_value=1):
+    """Generic input validation for numeric inputs (priority/place number)."""
+    while True:
+        try:
+            number = int(input(prompt).strip())
+            if number > min_value - 1:
+                return number
+            print(f"Number must be > {min_value - 1}")
+        except ValueError:
+            print("Invalid input; enter a valid number")
+
+
+def add_place(places):
+    """Add a new unvisited place to the in-memory list with full input validation."""
+    name = get_valid_input("Name: ")
+    country = get_valid_input("Country: ")
+    priority = get_valid_number("Priority: ")
+    places.append([name, country, priority, UNVISITED])
+    print(f"{name} in {country} (priority {priority}) added to Travel Tracker.")
+
+
 def main():
     """Main function: program entry point and menu loop."""
     print(f"Travel Tracker 1.0 - by Qiuhao Wu")
@@ -95,7 +125,9 @@ def main():
     while True:
         choice = input(">>> ").strip().upper()
         if choice == "D":
-            display_places(places)  # Add display function
+            display_places(places)
+        elif choice == "A":
+            add_place(places)  # Add add_place function
         elif choice == "Q":
             save_places(places)
             print(f"{len(places)} places saved to {CSV_FILE}")
